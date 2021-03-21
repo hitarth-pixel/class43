d8fcf4d35b1ab349f0a1c14662b125ea3625338a
hitarth-pixel <75882978+hitarth-pixel@users.noreply.github.com>
2021-03-21 16:54:35 +0530

    Add files via upload

---
+++ form.js
@@ -0,0 +1,16 @@
+class Form{
+    constructor(){
+        this.button=createButton('start');
+
+    }
+
+    display(){
+        
+        this.button.position(displayWidth/2+30,displayHeight/2);
+        /* this.button.mousePressed(()=>{
+
+                    background(levelsImg)
+        }) */
+    }
+
+}
